58a11b5dbfd2b4ae68f924f8994b93c2a3f73a81
Nickithine <dominique.ithina@gmail.com>
2023-01-05 18:31:08 -0300

    feat: Entendendo conceitos de props nos componentes

---
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,21 @@
+import { RepositoryItem } from "./RepositotyItem";
+
+const repository = {
+  name: "repository",
+  description: "Forms in React components",
+  link: "https://github.com/unform/unform"
+}
+
+export function RepositoryList() {
+  return(
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+      <ul>
+        <RepositoryItem repository={repository}/>
+        <RepositoryItem repository={repository}/>
+        <RepositoryItem repository={repository}/>
+        <RepositoryItem repository={repository}/>
+      </ul>
+    </section>
+  );
+}
