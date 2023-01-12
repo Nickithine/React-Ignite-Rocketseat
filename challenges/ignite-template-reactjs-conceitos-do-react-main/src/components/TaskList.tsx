989a667834e25a1357a55359c586d7fdd658a86f
Nickithine <dominique.ithina@gmail.com>
2023-01-12 20:32:21 -0300

    feat: Criado funcionalidade de completar tarefa ???
    Dúvida sobre gambiarra inserido para tirar depois. Porém está
    funcionando.

--- challenges/ignite-template-reactjs-conceitos-do-react-main/src/components/TaskList.tsx
+++ challenges/ignite-template-reactjs-conceitos-do-react-main/src/components/TaskList.tsx
@@ -29,6 +29,13 @@ export function TaskList() {
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    tasks.map((task) => {
+      if(task.id == id){
+        task.isComplete ? task.isComplete = false : task.isComplete = true;
+      }
+    })
+    setTasks([...tasks]); // Isso é Gambiarra??????????? Não estava atualizando a renderização, embora o array retornasse corretamente com seus objetos e atributos.
   }
 
   function handleRemoveTask(id: number) {
